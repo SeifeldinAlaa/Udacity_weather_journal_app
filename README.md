1eeced1d186841226a726b4618eb14dcba2aea99
Seif <seifeldin.alaa@gmail.com>
2020-12-10 21:45:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Weather-Journal App Project
+
+## Overview
+This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI. 
+
+## Instructions
+This will require modifying the `server.js` file and the `website/app.js` file. You can see `index.html` for element references, and once you are finished with the project steps, you can use `style.css` to style your application to customized perfection.
+
+## Extras
+If you are interested in testing your code as you go, you can use `tests.js` as a template for writing and running some basic tests for your code.
+
+## Summary
+
+This project is a JavaScript server and client side which gets the weather
+data from external API and then updates the html elements dynamically 
+using JavaScript.
+
+## How to run the project 
+
+In terminal run the server command : npm start 
+Then open the web browser and write localhost:8000
---
+++ website/app.js
@@ -0,0 +1,95 @@
+/* Global Variables */
+
+
+const zipCodeElement = document.getElementById('zip');
+const feelingsInputElement = document.getElementById('feelings');
+const dateElement = document.getElementById('date');
+const temperatureElement = document.getElementById('temp');
+const contentElement = document.getElementById('content');
+const generateButton = document.getElementById('generate');
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?units=metric&zip=';
+let apiKey = '&appid=1eca0b0e2410dec83d833cdc002bbf91';
+
+// Get weather data from OpenWeatherMap API
+const getWeatherFromExternalAPI = async () => {
+    try {
+        const zipCodeValue = zipCodeElement.value;
+        const response = await fetch(baseURL + zipCodeValue + apiKey);
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+// Prepare POST Data 
+const prepareData = async () => {
+    try {
+        const weatherData = await getWeatherFromExternalAPI();
+        if (weatherData && weatherData.main && weatherData.main.temp) {
+            const temperatureValue = weatherData.main.temp;
+            const feelingsValue = feelingsInputElement.value;
+            return { temperature: temperatureValue, feelings: feelingsValue, date: newDate };
+        } else {
+            console.log('no temperature');
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+// Save Data by posting to server
+const postData = async (url, data) => {
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'same-origin',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+        const result = await response.json();
+        console.log('postData: ' + JSON.stringify(result));
+        return result;
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+// Get Data from server
+const getData = async (url) => {
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+        console.log('getData: ' + JSON.stringify(result));
+        return result;
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+// generate button click event listener
+generateButton.addEventListener('click', async () => {
+    try {
+        const data = await prepareData();
+        if (data) {
+            const postResult = await postData('/data', data);
+            console.log('postResult message: ' + postResult.message);
+            const result = await getData('/all');
+            dateElement.innerHTML = 'Date: ' + result.date;
+            contentElement.innerHTML = 'I feel ' + result.feelings;
+            temperatureElement.innerHTML = 'Temperature: ' + result.temperature;
+        } else {
+            console.log('no data');
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+});
